137890867ba15cb19c2ee4370d35b0d4098a4d82
Siddharth J <jsiddharth80@gmail.com>
2018-11-26 18:30:31 +0530

    Delete defines.sv

--- testbench/defines.sv
+++
@@ -1,9 +0,0 @@
-`include "transaction.sv"
-`include "generator.sv"
-`include "intf.sv"
-`include "driver.sv"
-`include "environment.sv"
-`include "test.sv"
-`include "mux.v"
-`include "tbench_top.sv"
-
