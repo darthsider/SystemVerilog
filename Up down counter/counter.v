0031d7fc4ab58d7b13d50d01a41c693843a9d90a
Siddharth J <jsiddharth80@gmail.com>
2018-12-21 16:55:13 +0530

    Add files via upload

--- Up down counter/counter.v
+++ Up down counter/counter.v
@@ -1,21 +1,22 @@
-module counter(clk,reset,up_down,data_in,load,data_out);
-  
-  input clk;
-  input reset;
-  input up_down;
-  input load;
-  input [3:0] data_in;
-  output reg [3:0] data_out;
+module counter(clk,rst,load,updown,data,data_out);
+
+
+input clk;
+input rst;
+input load;
+input updown;
+input [3:0] data;
+output reg [3:0] data_out;
 
 
 always @(posedge clk) begin
-  if(reset)
+  if(rst)
     data_out <= 0;
   else if(load)
-    data_out <= data_in;
-  else if(up_down)
+    data_out <= data;
+  else if(updown)
     data_out <= data_out + 1;
-  else
+  else 
     data_out <= data_out - 1;
   end
   
